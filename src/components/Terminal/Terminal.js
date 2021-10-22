913d42bdd465198d2c6c53c5402351ceaf6c88c8
Desturo <lolomat25@gmai.com>
2021-10-22 11:29:17 +0200

    init

---
+++ src/components/Terminal/Terminal.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Terminal.css'
+
+const Terminal = () => {
+    return (
+        <div>
+            <textarea type="text" className="mainTerminal"/>
+        </div>
+    )
+}
+
+export default Terminal
