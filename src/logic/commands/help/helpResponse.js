7d07aa488f5e87ea198e64f4605cfa797197a365
Desturo <lolomat25@gmai.com>
2021-10-28 10:32:19 +0200

    help!!!

--- src/logic/commands/help/helpResponse.js
+++ src/logic/commands/help/helpResponse.js
@@ -2,6 +2,13 @@ import * as data from './helpValues.json'
 
 export const helpCommand = (inputArray) => {
     let text = "Here you'll get some help!"
+    if(inputArray[1] === undefined){
+        return { path: "nonexist", text: "gdfg{'\n'}flsdf{'\n'}kjjklj" }
+    }
     data.available.indexOf(inputArray[1]) !== -1 ? text = `Command help with parameter ${inputArray[1]}` : text = "No valid Parameter"
     return { path: "nonexist", text: text };
+}
+
+const name = (params) => {
+    
 }
